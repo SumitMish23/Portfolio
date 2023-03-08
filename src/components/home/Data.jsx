26a54fec172c74133f58e522e8948a1cb399c947
Sumit Mishra <sumit894sumit@gmail.com>
2023-03-08 21:29:17 +0530

    typewriter effect

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from "react";
 import { TbBrandTelegram } from "react-icons/tb";
-import './Home.css'
+import "./Home.css";
 
 const Data = () => {
+  const arrayOfTypewriter = ["Programmer", "Frontend Developer"];
+  const [text, setText] = useState("Frontend Developer");
+  
+  
+
+  
   return (
     <div className="home__data">
-        <h1 className='home__title '>Sumit Mishra</h1>
-        <h3 className="home__subtitle blinker anim-typewriter">Frontend Developer</h3>
-        <p className='home__description'>I'm a passionate frontend developer and UI/UX Designer.</p>
-        <a href="#contact" className='button button--flex'>Say Hello  <TbBrandTelegram className='telegram-icon'/></a>
+      <h1 className="home__title ">Sumit Mishra</h1>
+      <h3 className="home__subtitle blinker anim-typewriter">{text}</h3>
+      <p className="home__description">
+        I'm a passionate frontend developer and UI/UX Designer.
+      </p>
+      <a href="#contact" className="button button--flex">
+        Say Hello <TbBrandTelegram className="telegram-icon" />
+      </a>
     </div>
-  )
-}
+  );
+};
 
 export default Data;
