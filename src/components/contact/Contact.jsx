ed49d6402710a30b8de6ff70017ef25fa3f98ae5
Sumit Mishra <sumit894sumit@gmail.com>
2024-07-31 01:57:55 +0530

    done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -35,12 +35,14 @@ const Contact = () => {
 
         // Call API for sending email :
         const formData = new FormData(e.target);
+        const apiKey = 'why this kolaveri di';
         try {
             const data = await fetch("http://localhost:8000/contact", {
                 method: "POST",
                 body: JSON.stringify(Object.fromEntries(formData)),
                 headers: {
                     'Content-Type': 'application/json',
+                    'x-api-key': btoa(apiKey),
                 },
             })
 
@@ -132,7 +134,7 @@ const Contact = () => {
                     <span className="sr-only">Fire icon</span>
                 </div>
                 <div className="ms-3 text-sm font-normal">Email Sent Successfully !</div>
-                <button type="button" onClick={()=>setEmailModal(false)} className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-default" aria-label="Close">
+                <button type="button" onClick={() => setEmailModal(false)} className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-default" aria-label="Close">
                     <span className="sr-only">Close</span>
                     <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                         <path stroke="currentColor" stroke-linecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
